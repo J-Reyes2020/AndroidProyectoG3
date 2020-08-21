158dde009f317ba229b9540dcdf5cdec2e777f0a
Dilanomar <64571361+Dilanomar@users.noreply.github.com>
2020-08-21 10:34:51 -0600

    Modificaciones

--- app/src/main/java/com/example/proyectofinal/AdaptadorClienteRecycler.java
+++ app/src/main/java/com/example/proyectofinal/AdaptadorClienteRecycler.java
@@ -1,5 +1,6 @@
 package com.example.proyectofinal;
 
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -55,11 +56,17 @@ public class AdaptadorClienteRecycler extends RecyclerView.Adapter<AdaptadorClie
         public ViewHolderDatos(@NonNull View itemView) {
             super(itemView);
             tvCodigoCliente = (TextView) itemView.findViewById(R.id.tv_item_cliente_id);
+            tvCodigoCliente.setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
             tvNombre = (TextView) itemView.findViewById(R.id.tv_item_nombre_cliente);
+            tvNombre.setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
             tvApellido = (TextView) itemView.findViewById(R.id.tv_item_apellido_cliente);
+            tvApellido.setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
             tvTelefono = (TextView) itemView.findViewById(R.id.tv_item_telefono_cliente);
+            tvTelefono.setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
             tvDireccion = (TextView) itemView.findViewById(R.id.tv_item_direccion_cliente);
+            tvDireccion.setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
             tvCorreo = (TextView) itemView.findViewById(R.id.tv_item_correo_cliente);
+            tvCorreo.setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
         }
     }
 }
