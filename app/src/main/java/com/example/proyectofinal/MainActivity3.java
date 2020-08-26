60ed2d9b45d46af1983a5c460d1361e32b98973e
J-Reyes2020 <64215817+J-Reyes2020@users.noreply.github.com>
2020-08-25 18:21:24 -0600

    CmabiosNuevos

--- app/src/main/java/com/example/proyectofinal/MainActivity3.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity3.java
@@ -12,6 +12,7 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
     }
 
     public void onClicVerFactura(View view) {
